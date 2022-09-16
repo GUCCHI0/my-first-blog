d34db701e58affa2f9a5b91c17b2a6a6a49d7e99
GUCCHI0 <t.mizuguchi@estyle-inc.jp>
2022-09-16 13:49:19 +0900

    Added view and template for detailed blog post as well as CSS for the site.

---
+++ config/setting.py
@@ -0,0 +1,8 @@
+INSTALLED_APPS = [
+     # General use templates & template tags (should appear first)
+    'adminlte3',
+     # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
+
+    ...
+]
